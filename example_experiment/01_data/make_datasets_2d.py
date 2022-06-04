54dd299afb7435b215a124d0875a835a135da6d7
yajivunev <vvenu@utexas.edu>
2022-06-04 17:23:02 -0500

    make datasets_2d multiproc

--- example_experiment/01_data/make_datasets_2d.py
+++ example_experiment/01_data/make_datasets_2d.py
@@ -2,40 +2,54 @@ import daisy
 import numpy as np
 import sys
 import os
+import multiprocessing as mp
+
+
+def write_section(input_zarr,dataset,section,write_roi,write_vs,dtype,idx):
+   
+    print(f"at section {idx}..")
+
+    new_ds = daisy.prepare_ds(
+            input_zarr,
+            f"2d_{dataset}/{idx}",
+            write_roi,
+            write_vs,
+            dtype)
+
+    new_ds[write_roi] = section
 
 
 if __name__ == "__main__":
 
     input_zarr = sys.argv[1]
-    datasets_to_convert = sys.argv[2:]
+    dataset = sys.argv[2]
 
-    for dataset in datasets_to_convert:
+    print(f"making {dataset} 2d..")
+    ds = daisy.open_ds(input_zarr,dataset)
 
-        print(f"making {dataset} 2d..")
-        ds = daisy.open_ds(input_zarr,dataset)
+    ds_data = ds.to_ndarray()
 
-        ds_data = ds.to_ndarray()
+    roi = ds.roi
+    voxel_size = ds.voxel_size
+    dtype = ds.dtype
+ 
+    write_roi = daisy.Roi((0,0),roi.shape[1:])
+    write_vs = voxel_size[1:]
 
-        roi = ds.roi
-        voxel_size = ds.voxel_size
-        dtype = ds.dtype
+    with mp.Pool(2) as pool:
 
-        for id,sec in enumerate(ds_data):
-
-            if id % 50 == 0:
-                print(f"at section {id}..")
-
-            write_roi = daisy.Roi((0,0),roi.shape[1:])
-            #write_roi = daisy.Roi((0,0,0),(50,)+roi.shape[1:])
-            write_vs = voxel_size[1:]
-            #write_vs = voxel_size
-
-            new_ds = daisy.prepare_ds(
-                    input_zarr,
-                    f"2d_{dataset}/{id}",
-                    write_roi,
-                    write_vs,
-                    dtype)
-
-            new_ds[write_roi] = sec
-            #new_ds[write_roi] = np.expand_dims(sec,axis=0)
+        pool.starmap(write_section,[(input_zarr,dataset,section,write_roi,write_vs,dtype,idx) for idx,section in enumerate(ds_data)])
+#    for id,sec in enumerate(ds_data):
+#
+#        if id % 50 == 0:
+#            print(f"at section {id}..")
+#
+#        new_ds = daisy.prepare_ds(
+#                input_zarr,
+#                f"2d_{dataset}/{id}",
+#                write_roi,
+#                write_vs,
+#                dtype)
+#
+#        new_ds[write_roi] = sec
+#        #new_ds[write_roi] = np.expand_dims(sec,axis=0)
--- example_experiment/01_data/make_unlabelled.py
+++ example_experiment/01_data/make_unlabelled.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
 
     input_zarr = sys.argv[1]
     labels_ds = sys.argv[2]
+    out_ds_name = sys.argv[3]
 
     labels = daisy.open_ds(input_zarr,labels_ds)
 
@@ -16,6 +17,6 @@ if __name__ == "__main__":
     voxel_size = labels.voxel_size
     roi = labels.roi
 
-    unlabelled_ds = daisy.prepare_ds(input_zarr,'unlabelled',roi,voxel_size,dtype=np.uint8)
+    unlabelled_ds = daisy.prepare_ds(input_zarr,out_ds_name,roi,voxel_size,dtype=np.uint8)
 
     unlabelled_ds[roi] = unlabelled
